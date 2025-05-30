67e9e8dc29f52c1d614819adc3e29044205eabb4
cindybhuynh <cindybhuynh55@gmail.com>
2025-05-29 22:34:01 -0500

    Added description to README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # c-user-profile
+This is a basic user profile that utilizes user I/O.
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main (){
 
     printf("Welcome to the User Profile App! \n");
-    /*
+    
     // User i/o for full name
     char firstName[30];
     char lastName[30];
@@ -12,9 +12,6 @@ int main (){
     scanf("%s%s", firstName, lastName);
     printf("This is your user profile: \n\n");
     printf("Full Name: %s %s \n", firstName, lastName);
-    */
-
-
 
     // User i/o for age
     int age;
