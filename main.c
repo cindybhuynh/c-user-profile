a122bf5096dad8d15fd6f4ca56018905f4f14649
cindybhuynh <cindybhuynh55@gmail.com>
2025-05-29 22:28:56 -0500

    Added age user I/O

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main (){
 
     printf("Welcome to the User Profile App! \n");
-
+    /*
     // User i/o for full name
     char firstName[30];
     char lastName[30];
@@ -12,5 +12,20 @@ int main (){
     scanf("%s%s", firstName, lastName);
     printf("This is your user profile: \n\n");
     printf("Full Name: %s %s \n", firstName, lastName);
-    
+    */
+
+
+
+    // User i/o for age
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age < 0) {
+        printf("Invalid age entered. Please enter a valid age.\n");
+        return 1; // Exit the program with an error code
+    }
+    printf("Age: %d \n", age);
+
+
+    return 0;
 }
