fdb22469ed7249a21ad3d082bbdf0f5f88c91583
cindybhuynh <cindybhuynh55@gmail.com>
2025-06-01 20:34:51 -0500

    Fixed school summary + added gpa

--- main.c
+++ main.c
@@ -27,28 +27,40 @@ int main (){
     int gradeLevel;
     char universityLevel[10];
     char universityYear[5];
+
     printf("What type of school are you in? (Ex: K-12, college, university): ");
     scanf(" %s",schoolLevel);
     if (strcmp(schoolLevel, "K-12") == 0) {
-        printf("Enter your grade level (9, 10, 11, 12): ");
-        scanf("%d", &gradeLevel);
-        printf("Grade Level: %d\n", gradeLevel);    
+        printf("Enter your grade level (9, 10, 11, 12, etc.): ");
+        scanf("%d", &gradeLevel);  
     } else if (strcmp(schoolLevel, "college") == 0) {
         printf("Enter your college level (Freshman, Sophomore, Junior, etc.): ");
         scanf(" %s", collegeLevel);
-        printf("College Level: %s\n", collegeLevel);
     } else if (strcmp(schoolLevel, "university") == 0) {
         printf("Enter your university level (First year, Second year, Third year, etc.): ");
         scanf(" %s%s", universityLevel, universityYear);
-        printf("University Level: %s %s\n", universityLevel, universityYear);
     } else {
-        printf("Please enter a valid type of school: ");
+        printf("Invalid school type entered. Please enter a valid school type.\n");
+        return 1; // Exit the program
     }
     
+    // User I/O for gpa
+    double gpa;
+    printf("Enter your current gpa (Ex: 3.973): ");
+    scanf("%lf", &gpa);
+
     // User profile summary
     printf("This is your user profile: \n");
     printf("Full Name: %s %s\n", firstName, lastName);
     printf("Age: %d\n", age);
-    
+    if (strcmp(schoolLevel, "K-12") == 0) {
+        printf("Grade Level: %d\n", gradeLevel);
+    } else if (strcmp(schoolLevel, "college") == 0) {
+        printf("College Year: %s\n", collegeLevel);
+    } else if (strcmp(schoolLevel, "university") == 0) {
+        printf("University Year: %s %s\n", universityLevel, universityYear);
+    }
+    printf("GPA: %.3f\n", gpa);
+
     return 0;
 }
